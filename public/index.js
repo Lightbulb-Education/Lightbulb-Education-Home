b79be4eac71db6e7f9d9ceec696aa9c86b1cdb7a
Adam Nguyen <nguyenadam5@yahoo.com>
2020-05-17 14:53:32 -0500

    Added about page

--- public/index.js
+++ public/index.js
@@ -13,7 +13,7 @@ const homeContent = Vue.component('home-hero-content', {
           </h1>
           <div class="buttons is-centered">
             <a class="button is-light is-success is-rounded" href="https://code.lightbulb.education" target="_top">Lightbulb Edu Code</a>
-            <router-link class="button is-light is-primary is-rounded" to="/formsplus">Forms+</router-link>  
+            <router-link class="button is-light is-primary is-rounded" to="/formsplus">Forms+</router-link>
             <router-link class="button is-light is-link is-rounded" to="/pd">Past PD Sessions</router-link>
           </div>
         </div>
@@ -21,7 +21,7 @@ const homeContent = Vue.component('home-hero-content', {
     </section>`
 })
 
-const formsPlus = Vue.component('forms-plus-content', {
+const formsPlusContent = Vue.component('forms-plus-content', {
   template: `<section class="hero is-success is-fullheight-with-navbar">
       <!-- Hero content: will be in the middle -->
       <div class="hero-body">
@@ -29,11 +29,11 @@ const formsPlus = Vue.component('forms-plus-content', {
           <h1 class="title is-2">
             Forms+ is a Chrome extension that provides quality of life improvements for Google Forms.
           </h1>
-          
+
           <h3 class="title is-4">
             This includes the ability to eliminate answers for test/quiz scenarios, unselecting radio buttons by clicking on them again, and see a character count for text input questions.
           </h3>
-   
+
           <div class="buttons is-centered">
             <a class="button is-link is-rounded">
               <span class="icon">
@@ -52,6 +52,52 @@ const formsPlus = Vue.component('forms-plus-content', {
         </div>
       </div>
     </section>`
+})
+
+const aboutContent = Vue.component('about-content', {
+  template: `<section class="hero is-success is-fullheight-with-navbar">
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered content">
+        <h1 class="title is-2">
+          Lightbulb Education is an educational services company focused on bringing effective technology use to school classrooms.
+        </h1>
+
+        <h3 class="title is-4">
+          Founded by two high school students, we create technology based solutions for schools, especially in regards to helping students learn to code and helping teachers use technology to better impact their students.
+        </h3>
+
+        <div>
+          <h3 class="title is-4">
+            If you're a student interested in learning how to code, check out Lightbulb Edu Code! It's an online learning platform centered around learning by creating projects.
+          </h3>
+          <a class="button is-rounded" href="https://code.lightbulb.education/">
+            <span>Learn to code!</span>
+          </a>
+        </div>
+
+        <div>
+          <h3 class="title is-4">
+            If you're a technology education specialist and you want to see how teachers can better use technology in the classroom, feel free to reach out to us! We do consulting as well as professional development training sessions.
+          </h3>
+          <a class="button is-rounded" href="mailto:contact@lightbulb.education">
+            <span>Contact us</span>
+          </a>
+        </div>
+
+        <div>
+          <h3 class="title is-4">
+            And if you're a human and you use Google Forms, check out our Google Chrome extension Forms+. This extension adds several quality of life improvements to Google Forms that you're sure to enjoy.
+          </h3>
+          <a class="button is-rounded" href="https://chrome.google.com/webstore/category/extensions">
+            <span>Get Forms+</span>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+`
 })
 
 Vue.component('nav-bar', {
@@ -90,7 +136,8 @@ Vue.component('nav-bar', {
 const routes = [
   { path: '/', component: homeContent },
   { path: '/home', component: homeContent },
-  { path: '/formsplus', component: formsPlus },
+  { path: '/formsplus', component: formsPlusContent },
+  { path: '/about', component: aboutContent}
   // { path: '/bar', component: Bar }
 ]
 
